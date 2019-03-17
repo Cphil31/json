6d85229e355d0fae9e119b3663eb6542b84a3352
unknown <cecilephilippe31@gmail.com>
2019-03-17 15:41:29 +0100

    recevoir les données et la convertir en fonction javascript , utiliser la fonction parse()

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 console.log("hello philippe");
 // creation d'un tableau json
-var id = {
-  "name":"Alex",
-  "age":26,
-  "sexe":"homme",
-  "profession":"instructeur",
-  // appeler un array
-  "hobby":["lecture","Basket", null],
-  // création d'un object json
-  "voitures" : {
-    "voiture1": "BMW",
-    "voiture2": "Audi",
-  }
-}
+var id = '{"name":"alex","age":26,"sexe":"homme","profession":"instructeur","Hobby":["Lecture","Basket",null]}';
+
+var json = '{"result":true, "count":42}';
+
+var Tojs = JSON.parse(id);
+
 // afficher dans la console la valeur de l'objet :
 // ex :
 // taper dans la console : id.name
@@ -26,3 +19,8 @@ var id = {
 // ex:
 // taper dans la console : id.voitures.voiture1
 // resultat "BMW"
+
+// recevoir les données et la convertir en fonction javascript , utiliser la fonction parse()
+// taper Tojs
+// converti en javasript :
+// Object { name: "alex", age: 26, sexe: "homme", profession: "instructeur", Hobby: (3) […] }
